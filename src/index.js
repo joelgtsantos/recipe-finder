e76036108ceeeb5a9968316586f2fcbe5b96fc05
Joel Santos <joelgtsantos@gmail.com>
2018-05-10 17:49:52 -0600

    Add router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import FavoriteRecipeList from './components/FavoriteRecipeList';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('state', store.getState()));
+
+ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={App} />
+          <Route path='/favorites' component={FavoriteRecipeList}/>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
